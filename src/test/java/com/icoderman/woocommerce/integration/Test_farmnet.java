51ea620c9780b56e8f064357c15ee806251b425a
Винников Алексей <a89517712996@gmail.com>
2021-04-28 14:05:21 +0300

    Работающий прототип

--- src/test/java/com/icoderman/woocommerce/integration/Test_farmnet.java
+++ src/test/java/com/icoderman/woocommerce/integration/Test_farmnet.java
@@ -1,11 +1,14 @@
 package com.icoderman.woocommerce.integration;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
@@ -20,40 +23,107 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Test_farmnet {
 
-	static WooCommerceClientTest woo = new WooCommerceClientTest();
+	static WooCommerceClientTest woo;
 	static String token;
-	//Хэш с номерами товаров и их описанием
+	// Хэш с номерами товаров и их описанием
 	static HashMap<String, LinkedHashMap> item_map = new LinkedHashMap<String, LinkedHashMap>();
-	//Хэш с номерами товаров и признаком наличия
-	static HashMap<String, Boolean> item_exists = new LinkedHashMap<String, Boolean>(); 
-
-	Test_farmnet() {
-		try {
-			token = login("32201", "DA2IWMLS");
-			woo.setUp();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	// Хэш с номерами загруженных товаров товаров
+	static HashMap<String, LinkedHashMap> woo_item_map = new LinkedHashMap<String, LinkedHashMap>();
+	// Список логинов
+	static HashMap<String, String> users = new LinkedHashMap<String, String>();
 
 	public static void main(String[] args) throws ClientProtocolException, IOException {
 
-		//String token = login("32201", "DA2IWMLS");
-		WooCommerceClientTest woo = new WooCommerceClientTest();
-/*
-		// Получаем список филиалов
-		LinkedHashMap branch = branchList(token);
-		String branchList = "";
-		// Узнаем остатки по филиалам и заполняем item_map
-		for (LinkedHashMap item : (List<LinkedHashMap>) branch.get("items")) {
-			LinkedHashMap ost = ostByDate(token, Integer.toString((int) item.get("branchId")));
-			for (LinkedHashMap ost_item : (List<LinkedHashMap>) ost.get("items")) {
-				item_map.put(Integer.toString((Integer)ost_item.get("regId")), ost_item);
+		woo = new WooCommerceClientTest();
+		woo.setUp();
+
+		try {
+			FileInputStream fis = new FileInputStream("users");
+			Scanner sc = new Scanner(fis);
+			String login = "";
+			String password = "";
+			while (sc.hasNext()) {
+				login = sc.next();
+				password = sc.next();
+				users.put(login, password);
 			}
-		}*/
-		
-		List wooProducts = wooGetAllProducts();
-		int i = 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		for (String login : users.keySet()) {
+			String token = login(login, users.get(login));
+
+			// Получаем список филиалов
+			LinkedHashMap branch = branchList(token);
+			String branchList = "";
+			// Узнаем остатки по филиалам и заполняем item_map
+			for (LinkedHashMap item : (List<LinkedHashMap>) branch.get("items")) {
+				LinkedHashMap ost = ostByDate(token, Integer.toString((int) item.get("branchId")));
+				for (LinkedHashMap ost_item : (List<LinkedHashMap>) ost.get("items")) {
+					item_map.put(Integer.toString((Integer) ost_item.get("regId")), ost_item);
+				}
+			}
+
+			// Получаем список загруженных товаров
+			List<LinkedHashMap> wooAllProducts = wooGetAllProducts();
+			for (LinkedHashMap item : wooAllProducts) {
+				woo_item_map.put((String) item.get("sku"), item);
+			}
+
+			if (wooAllProducts.isEmpty())
+				wooAllProducts = new ArrayList<LinkedHashMap>();
+
+			// Сравниваем две таблицы и заполняем запрос на запись
+			int count = 0;
+			List<Map<String, Object>> create_list = new ArrayList<>();
+			List<Map<String, Object>> update_list = new ArrayList<>();
+			for (String key : item_map.keySet()) {
+				count++;
+				if (woo_item_map.containsKey(key)) {
+					Map<String, Object> product = new HashMap<>();
+					product.put("sku", item_map.get(key).get("regId"));
+					product.put("name", item_map.get(key).get("tovName"));
+					product.put("short_description", item_map.get(key).get("fabr"));
+					product.put("stock_quantity", item_map.get(key).get("uQntOst"));
+					product.put("regular_price", item_map.get(key).get("priceRoznWNDS"));
+
+					update_list.add(product);
+				} else {
+					Map<String, Object> product = new HashMap<>();
+					product.put("sku", item_map.get(key).get("regId"));
+					product.put("name", item_map.get(key).get("tovName"));
+					product.put("short_description", item_map.get(key).get("fabr"));
+					product.put("stock_quantity", item_map.get(key).get("uQntOst"));
+					product.put("regular_price", item_map.get(key).get("priceRoznWNDS"));
+
+					create_list.add(product);
+				}
+				if (count == 99) {
+					Map<String, Object> reqOptions = new HashMap<>();
+
+					if (!create_list.isEmpty())
+						reqOptions.put("create", create_list);
+					if (!update_list.isEmpty())
+						reqOptions.put("update", update_list);
+
+					wooBatchProduct(reqOptions);
+
+					count = 0;
+					create_list.clear();
+					update_list.clear();
+				}
+			}
+
+			Map<String, Object> reqOptions = new HashMap<>();
+
+			if (!create_list.isEmpty())
+				reqOptions.put("create", create_list);
+			if (!update_list.isEmpty())
+				reqOptions.put("update", update_list);
+
+			wooBatchProduct(reqOptions);
+		}
 	}
 
 	public static String login(String customerId, String password) throws IOException {
@@ -199,16 +269,28 @@ public class Test_farmnet {
 	}
 
 	public static List wooGetAllProducts() {
+		List answer = new ArrayList();
 		List result = null;
+		int page = 1;
 		try {
-			result = woo.apiGetAllProductsTest();
+			do {
+				result = woo.apiGetAllProductsTest(Integer.toString(page));
+				answer.addAll(result);
+				page++;
+			} while (!result.isEmpty());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return result;
+		return answer;
 	}
-	
-	
+
+	public static void wooBatchProduct(Map<String, Object> reqOptions) {
+		try {
+			woo.apiBatchProductTest(reqOptions);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	static class Login_entity {
 		public String status;
--- src/test/java/com/icoderman/woocommerce/integration/WooCommerceClientTest.java
+++ src/test/java/com/icoderman/woocommerce/integration/WooCommerceClientTest.java
@@ -1,6 +1,5 @@
 package com.icoderman.woocommerce.integration;
 
-
 import com.fasterxml.jackson.databind.Module.SetupContext;
 import com.icoderman.woocommerce.ApiVersionType;
 import com.icoderman.woocommerce.EndpointBaseType;
@@ -19,93 +18,91 @@ import java.util.Map;
 
 public class WooCommerceClientTest {
 
-    private static final String CONSUMER_KEY = "ck_bf2f38bf4c72ff1fd28bdb8e2b3e694314c894d1";
-    private static final String CONSUMER_SECRET = "cs_2ed801ca381abbafa2d38d43e202dc4a4a0c72cc";
-    private static final String WC_URL = "http://aptekawatson.ru";
+	private static final String CONSUMER_KEY = "ck_bf2f38bf4c72ff1fd28bdb8e2b3e694314c894d1";
+	private static final String CONSUMER_SECRET = "cs_2ed801ca381abbafa2d38d43e202dc4a4a0c72cc";
+	private static final String WC_URL = "http://aptekawatson.ru";
 
-    private WooCommerce wooCommerce;
+	private WooCommerce wooCommerce;
 
-    @Before
-    public void setUp() {
-        wooCommerce = new WooCommerceAPI(new OAuthConfig(WC_URL, CONSUMER_KEY, CONSUMER_SECRET), ApiVersionType.V2);
-    }
+	@Before
+	public void setUp() {
+		wooCommerce = new WooCommerceAPI(new OAuthConfig(WC_URL, CONSUMER_KEY, CONSUMER_SECRET), ApiVersionType.V2);
+	}
 
-    @Ignore
-    @Test
-    public void apiCreateProductTest(Map<String, Object> productInfo) {
-        Map product = wooCommerce.create(EndpointBaseType.PRODUCTS.getValue(), productInfo);
-        Assert.assertNotNull(product);
-    }
+	@Ignore
+	@Test
+	public void apiCreateProductTest(Map<String, Object> productInfo) {
+		Map product = wooCommerce.create(EndpointBaseType.PRODUCTS.getValue(), productInfo);
+		Assert.assertNotNull(product);
+	}
 
-    @Ignore
-    @Test
-    public List apiGetAllProductsTest() {
-        Map<String, String> params = new HashMap<>();
-        params.put("per_page","1000");
-        params.put("offset","0");
-        Object products = wooCommerce.getAll(EndpointBaseType.PRODUCTS.getValue(), params);
-        Assert.assertNotNull(products);
-        return (List)products;
-    }
+	@Ignore
+	@Test
+	public List apiGetAllProductsTest(String page) {
+		Map<String, String> params = new HashMap<>();
+		params.put("per_page", "100");
+		params.put("page", page);
+		Object products = wooCommerce.getAll(EndpointBaseType.PRODUCTS.getValue(), params);
+		Assert.assertNotNull(products);
+		return (List) products;
+	}
 
-    @Ignore
-    @Test
-    public void apiGetProductTest() {
-        Map product = wooCommerce.get(EndpointBaseType.PRODUCTS.getValue(), 79);
-        Assert.assertNotNull(product);
-    }
+	@Ignore
+	@Test
+	public void apiGetProductTest() {
+		Map product = wooCommerce.get(EndpointBaseType.PRODUCTS.getValue(), 79);
+		Assert.assertNotNull(product);
+	}
 
-    @Ignore
-    @Test
-    public void apiUpdateProductTest(Map<String, Object> productInfo) {
-        Map product = wooCommerce.update(EndpointBaseType.PRODUCTS.getValue(), 10, productInfo);
-        Assert.assertNotNull(product);
-    }
+	@Ignore
+	@Test
+	public void apiUpdateProductTest(Map<String, Object> productInfo) {
+		Map product = wooCommerce.update(EndpointBaseType.PRODUCTS.getValue(), 10, productInfo);
+		Assert.assertNotNull(product);
+	}
 
-    @Ignore
-    @Test
-    public void apiDeleteProductTest() {
-        Map product = wooCommerce.delete(EndpointBaseType.PRODUCTS.getValue(), 10);
-        Assert.assertNotNull(product);
-    }
+	@Ignore
+	@Test
+	public void apiDeleteProductTest() {
+		Map product = wooCommerce.delete(EndpointBaseType.PRODUCTS.getValue(), 10);
+		Assert.assertNotNull(product);
+	}
 
-    @Ignore
-    @Test
-    public void apiBatchVariationTest() {
-        Integer productId = 20072;
+	@Ignore
+	@Test
+	public void apiBatchVariationTest() {
+		Integer productId = 20072;
 
-        List<Map<String, Object>> variations = new ArrayList<>();
-        for (int variationId = 20073; variationId <= 20081; variationId++){
-            Map<String, Object> variation = new HashMap<>();
-            variation.put("id", variationId+"");
-            variation.put("regular_price", "10");
-            variations.add(variation);
-        }
+		List<Map<String, Object>> variations = new ArrayList<>();
+		for (int variationId = 20073; variationId <= 20081; variationId++) {
+			Map<String, Object> variation = new HashMap<>();
+			variation.put("id", variationId + "");
+			variation.put("regular_price", "10");
+			variations.add(variation);
+		}
 
-        Map<String, Object> reqOptions = new HashMap<>();
-        reqOptions.put("update", variations);
+		Map<String, Object> reqOptions = new HashMap<>();
+		reqOptions.put("update", variations);
 
-        Map response = wooCommerce.batch(String.format("products/%d/variations", productId), reqOptions);
-        Assert.assertNotNull(response);
-    }
+		Map response = wooCommerce.batch(String.format("products/%d/variations", productId), reqOptions);
+		Assert.assertNotNull(response);
+	}
 
-    @Ignore
-    @Test
-    public void apiBatchProductTest(Map<String, Object> reqOptions) {
+	@Ignore
+	@Test
+	public void apiBatchProductTest(Map<String, Object> reqOptions) {
 
-        /*List<Map<String, Object>> products = new ArrayList<>();
-        Map<String, Object> product = new HashMap<>();
-        product.put("id", "19916");
-        product.put("name", "MODIFIED NAME");
-        products.add(product);
+		/*
+		 * List<Map<String, Object>> products = new ArrayList<>(); Map<String, Object>
+		 * product = new HashMap<>(); product.put("id", "19916"); product.put("name",
+		 * "MODIFIED NAME"); products.add(product);
+		 * 
+		 * Map<String, Object> reqOptions = new HashMap<>(); reqOptions.put("update",
+		 * products);
+		 */
 
-        Map<String, Object> reqOptions = new HashMap<>();
-        reqOptions.put("update", products);*/
-    	
-    	
-
-        Map response = wooCommerce.batch(EndpointBaseType.PRODUCTS.getValue(), reqOptions);
-        Assert.assertNotNull(response);
-    }
+		wooCommerce.batch(EndpointBaseType.PRODUCTS.getValue(), reqOptions);
+		// Assert.assertNotNull(response);
+	}
 
 }
